a20070af72e62312ef264f68df6dda17047ae898
gzambotti <g.zambotti@gmail.com>
2024-10-30 15:35:49 -0400

    modify setup file

--- setup_env.bat
+++ setup_env.bat
@@ -1,21 +1,23 @@
 @echo off
 :: Set environment name and required libraries
-set ENV_NAME=pdf2jp2_test
-set REQUIRED_LIBS="pypdf pyqt pdf2image pillow"  :: Add your required libraries here
-set REQUIRED_LIBS_PIP="PyExifTool jpylyzer"
+set ENV_NAME=ENV_NAME
 :: Create the conda environment
 call C:\ProgramData\Miniconda3\Scripts\conda.exe create -n %ENV_NAME% python=3.9 -y
 
 :: Activate the environment
 call C:\ProgramData\Miniconda3\Scripts\activate.bat %ENV_NAME%
 
-:: Install required libraries
-call C:\ProgramData\Miniconda3\Scripts\conda.exe install %REQUIRED_LIBS% -y
-
-call C:\ProgramData\Miniconda3\Scripts\pip.exe install %REQUIRED_LIBS_PIP% -y
 :: Optional: Install additional packages with pip if not available via Conda
 :: Uncomment the line below if needed
 :: pip install package_name
+
+pip install PyExifTool
+pip install jpylyzer
+pip install pypdf
+pip install PyQt5
+pip install pdf2image
+pip install pillow
+
 :: conda remove --name ENVIRONMENT --all
 echo Environment %ENV_NAME% created and packages installed successfully.
 pause
